d1eb325037b993aa0f59022e05819d349f4544eb
Ripley-426 <nkotolenko@gmail.com>
2022-01-14 21:07:09 -0300

    finished example

--- src/main/kotlin/models/Customer.kt
+++ src/main/kotlin/models/Customer.kt
@@ -1,3 +1,8 @@
 package models
 
-data class Customer()
+import kotlinx.serialization.Serializable
+
+val customerStorage = mutableListOf<Customer>()
+
+@Serializable
+data class Customer(val id:String, val firstName:String, val email:String)
